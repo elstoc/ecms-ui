1409687f9f86c5262f4fc6b5aaad1d2d93d7bd9f
Chris Elston <chris.elston@blueyonder.co.uk>
2022-10-25 22:15:33 +0100

    Add initial draft of justified gallery
    - Uses sample JSON instead of connecting to API
    - Uses divs instead of images
    - Not very performant (needs to resize less frequently on drag)
    
    Resolves #3

---
+++ src/components/gallery/galleryPage.tsx
@@ -0,0 +1,77 @@
+import './galleryPage.css';
+import { useResizeDetector } from 'react-resize-detector';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import jsonData from './initialData.json';
+
+type Image = {
+    path: string;
+    width: number;
+    height: number;
+};
+
+const GalleryPage = () => {
+    const { width, height, ref } = useResizeDetector();
+    const border = 1;
+    const margin = 5;
+    const images = jsonData as Image[];
+    const [sizedImages, setSizedImages] = useState<Image[]>();
+
+    console.log(`width: ${width} height: ${height}`);
+
+    const redraw = useCallback((): void => {
+        let rowWidth = 0;
+        let rowImages: Image[] = [];
+        const newSizedImages: Image[] = [];
+
+        images.forEach((image) => {
+            rowImages.push(image);
+            rowWidth += image.width;
+            const desiredWidth = width! - 2 * (margin + border) * rowImages.length;
+            if(rowWidth === desiredWidth) {
+                newSizedImages.push(...rowImages);
+                rowImages = [];
+                rowWidth = 0;
+                console.log('desired');
+            } else if (rowWidth > desiredWidth) {
+                const ratio = desiredWidth / rowWidth;
+                rowImages = rowImages.map((image) => {
+                    return {
+                        path: image.path,
+                        width: Math.trunc(image.width * ratio),
+                        height: Math.trunc(image.height * ratio)
+                    };
+                });
+                newSizedImages.push(...rowImages);
+                rowImages = [];
+                rowWidth = 0;
+            }
+        });
+
+        newSizedImages.push(...rowImages);
+        setSizedImages(newSizedImages);
+    }, [width, images]);
+
+    useEffect(() => {
+        redraw();
+    },[redraw]);
+
+    const elements: JSX.Element[] = [];
+
+    sizedImages && sizedImages.forEach((item) => {
+        const style = {
+            width: `${item.width}px`,
+            height: `${item.height}px`,
+            border: `${border}px solid black`,
+            margin: `${margin}px`
+        };
+        elements.push(<div key={item.path} style={style}>Helloo World</div>);
+    });
+
+    return (
+        <div ref={ref} className="outerDiv">
+            {sizedImages ? elements : 'This item has not been rendered yet'}
+        </div>
+    );
+};
+
+export default GalleryPage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import './index.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import GalleryPage from './components/gallery/galleryPage';
 
 const container = document.getElementById('app-root')!;
 const root = createRoot(container);
-root.render(<h1>Hello React!!</h1>);
+root.render(<GalleryPage />);
