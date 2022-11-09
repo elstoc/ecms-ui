abb359d56939060ee6c72b40c7084ceae7cb1de4
Chris Elston <chris.elston@blueyonder.co.uk>
2022-11-09 23:13:36 +0000

    LightBox: Tidy up import

--- src/components/gallery/LightBox.tsx
+++ src/components/gallery/LightBox.tsx
@@ -1,6 +1,5 @@
 import React, { FC, ReactElement } from 'react';
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { ImageData } from './IGallery';
 
 import './LightBox.css';
