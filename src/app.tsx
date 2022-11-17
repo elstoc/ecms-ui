ec25b69e164a251db1f017843afbcba0670ea07f
Chris Elston <chris.elston@blueyonder.co.uk>
2022-11-17 08:02:38 +0000

    Another step towards an independent UI
    All routes and props now taken from an array, which could
    be delivered by an API GET endpoint

--- src/app.tsx
+++ src/app.tsx
@@ -2,28 +2,46 @@ import React, { ReactElement } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
 import './app.css';
-import Gallery from './components/gallery/Gallery';
-import Markdown from './components/markdown/Markdown';
+import Gallery, { GalleryProps } from './components/gallery/Gallery';
+import Markdown, { MarkdownProps } from './components/markdown/Markdown';
+
+type SiteProps = (GalleryProps & {type: 'gallery'} | MarkdownProps & {type: 'markdown'})[];
+
+const siteProps: SiteProps = [
+    {
+        path: 'portfolio',
+        margin: 3,
+        type: 'gallery',
+    },
+    {
+        path: 'markdown-testing',
+        type: 'markdown'
+    }
+];
 
 const App = (): ReactElement => {
+    const siteRoutes = siteProps.map((props) => {
+        let element: ReactElement;
+        if (props.type === 'gallery') {
+            element = <Gallery path={props.path} margin={props.margin} />;
+        } else {
+            element = <Markdown path={props.path} />;
+        }
+        return (
+            <Route
+                key={props.path}
+                path={`${props.path}/*`}
+                element={element}
+            />
+        );
+    });
     return (
         <>
             <header>
                 Chris Elston's Home Page
             </header>
             <Routes>
-                <Route
-                    path="portfolio/*"
-                    element={<Gallery path="portfolio" margin={3} />}
-                />
-                <Route
-                    path="markdown-testing/*"
-                    element={<Markdown path='markdown-testing' />}
-                />
-                <Route
-                    path="*"
-                    element={<Navigate to="portfolio" replace />}
-                />
+                {siteRoutes}
             </Routes>
             <footer>
                 2022
