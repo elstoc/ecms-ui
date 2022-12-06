e21e28ad62b46926dd4532c81acc7866240b48c4
Chris Elston <chris.elston@blueyonder.co.uk>
2022-12-06 14:00:47 +0000

    MarkdownPage: Improve formatting
    Resolves #48

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 
 import './app.css';
+import '../node_modules/modern-normalize/modern-normalize.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import SiteRoutes from './components/SiteRoutes';
