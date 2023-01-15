c9db4c00043a842a270e31f4a5545a4b8e081417
Chris Elston <chris.elston@blueyonder.co.uk>
2023-01-15 22:28:41 +0000

    Copy code from api

--- src/utils/splitFrontMatter.ts
+++ src/utils/splitFrontMatter.ts
@@ -1,11 +1,11 @@
-export const splitFrontMatter = (file: string): [yaml: string, content: string] => {
-    const lines = file.split('\n');
+export const splitFrontMatter = (text: string): [yaml: string, content: string] => {
+    const lines = text.split('\n');
     if (lines[0] !== '---') {
-        return ['', file];
+        return ['', text];
     } else {
         const endIndex = lines.indexOf('---', 1);
         if (endIndex === -1) {
-            return ['', lines.slice(1).join('\n')];
+            return [lines.slice(1).join('\n'), ''];
         } else {
             return [lines.slice(1, endIndex).join('\n'), lines.slice(endIndex + 1).join('\n')];
         }
