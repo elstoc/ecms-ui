357e9d3038c87625d8d3d4ca5510342b0fe481df
Chris Elston <chris.elston@blueyonder.co.uk>
2023-01-19 23:09:51 +0000

    Header: Remove unnecessary variable assignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,12 +9,11 @@ type HeaderProps = {
 };
 
 export const Header: FC<HeaderProps> = ({ siteProps }): ReactElement => {
-    const headerLinks = siteProps.map((props) =>
-        <NavLink to={props.path} key={props.path}>{props.title}</NavLink>
-    );
     return (
         <div className='header-links'>
-            {headerLinks}
+            {siteProps.map((props) =>
+                <NavLink to={props.path} key={props.path}>{props.title}</NavLink>
+            )}
         </div>
     );
 };
