c5ddd86fd1ce3fcbeb1790dc70b23082e63a826f
Chris Elston <chris.elston@blueyonder.co.uk>
2023-01-21 13:54:37 +0000

    Gallery: Separate data retrieval and rendering

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,11 +1,8 @@
 import { useResizeDetector } from 'react-resize-detector';
-import React, { FC, ReactElement, useCallback, useState, createRef } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { FC, ReactElement, useCallback, useState } from 'react';
 
-import { useIsVisible } from '../../hooks/useIsVisible';
 import { useGalleryList } from '../../hooks/galleryQueries';
-import { GalleryThumb } from './GalleryThumb';
-import { LightBox } from './LightBox';
+import { GalleryContent } from './GalleryContent';
 import './Gallery.css';
 import { GalleryData } from '../../types/Gallery';
 
@@ -18,10 +15,9 @@ export type GalleryProps = {
 }
 
 export const Gallery: FC<GalleryProps> = ({ path, marginPx, title, batchSize, threshold }): ReactElement => {
-    let resizeRatios: number[] = [];
     let message = '';
+    let showGallery = false;
 
-    const refTriggerLoadWhenVisible = createRef<HTMLImageElement>();
     const [maxImagesToLoad, setMaxImagesToLoad] = useState(batchSize);
     const [galleryDivWidth, setGalleryDivWidth] = useState(0);
 
@@ -30,8 +26,6 @@ export const Gallery: FC<GalleryProps> = ({ path, marginPx, title, batchSize, th
     const loadMoreImages = useCallback(() => {
         setMaxImagesToLoad((prevMaxImages) => (prevMaxImages < galleryData!.imageCount) ? (prevMaxImages + batchSize) : prevMaxImages);
     }, [galleryData, batchSize]);
-
-    useIsVisible(refTriggerLoadWhenVisible, loadMoreImages);
 
     const onResize = useCallback((width?: number) => (
         width && setGalleryDivWidth(width)
@@ -42,10 +36,10 @@ export const Gallery: FC<GalleryProps> = ({ path, marginPx, title, batchSize, th
         onResize
     });
 
-    if (error) {
+    if (galleryData && galleryDivWidth) {
+        showGallery = true;
+    } else if (error) {
         message = 'There has been an ERROR';
-    } else if (galleryData && galleryDivWidth) {
-        resizeRatios = getResizeRatios(galleryData, galleryDivWidth, marginPx);
     } else {
         message = 'Loading Images';
     }
@@ -54,57 +48,18 @@ export const Gallery: FC<GalleryProps> = ({ path, marginPx, title, batchSize, th
         <div className="galleryContainer">
             <div ref={widthRef} className="justifiedGallery">
                 {message}
-                {galleryData && (
-                    <Routes>
-                        <Route
-                            path=":imageName"
-                            element={
-                                <LightBox
-                                    path={path}
-                                    galleryData={galleryData}
-                                    loadMoreImages={loadMoreImages}
-                                />
-                            }
-                        />
-                    </Routes>
-                )}
-                {galleryData?.imageList?.map((image, index) => (
-                    <GalleryThumb
-                        key={image.fileName}
-                        image={image}
-                        title={`${title} - ${image.fileName}`}
-                        marginPx={marginPx}
-                        resizeRatio={resizeRatios[index] || 1}
+                {showGallery && (
+                    <GalleryContent
                         path={path}
-                        ref={
-                            index === galleryData.imageList.length - threshold
-                                ? refTriggerLoadWhenVisible
-                                : null
-                        }
+                        title={title}
+                        galleryData={galleryData as GalleryData}
+                        galleryDivWidth={galleryDivWidth}
+                        loadMoreImages={loadMoreImages}
+                        marginPx={marginPx}
+                        threshold={threshold}
                     />
-                ))}
+                )}
             </div>
         </div>
     );
-};
-
-const getResizeRatios = (galleryData: GalleryData, divWidth: number, marginPx: number): number[] => {
-    let nextRowWidthOfThumbs = 0;
-    let nextRowImageCount = 0;
-    const ratios: number[] = [];
-
-    galleryData?.imageList?.forEach((image) => {
-        nextRowImageCount++;
-        nextRowWidthOfThumbs += image.thumbDimensions.width;
-        const widthAvailableForThumbs = divWidth - (2 * marginPx * nextRowImageCount);
-
-        if (nextRowWidthOfThumbs >= widthAvailableForThumbs) {
-            const resizeRatio = widthAvailableForThumbs / nextRowWidthOfThumbs;
-            ratios.push(...Array(nextRowImageCount).fill(resizeRatio));
-            nextRowWidthOfThumbs = 0;
-            nextRowImageCount = 0;
-        }
-    });
-
-    return ratios;
 };
---
+++ src/components/gallery/GalleryContent.tsx
@@ -0,0 +1,79 @@
+import React, { FC, ReactElement, createRef } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { useIsVisible } from '../../hooks/useIsVisible';
+import { GalleryThumb } from './GalleryThumb';
+import { LightBox } from './LightBox';
+import './Gallery.css';
+import { GalleryData } from '../../types/Gallery';
+
+export type GalleryContentProps = {
+    galleryData: GalleryData;
+    galleryDivWidth: number;
+    loadMoreImages: () => void;
+    path: string;
+    title: string;
+    marginPx: number;
+    threshold: number;
+}
+
+export const GalleryContent: FC<GalleryContentProps> = ({ galleryData, galleryDivWidth, loadMoreImages, path, marginPx, title, threshold }): ReactElement => {
+    const refTriggerLoadWhenVisible = createRef<HTMLImageElement>();
+
+    useIsVisible(refTriggerLoadWhenVisible, loadMoreImages);
+
+    const resizeRatios = getResizeRatios(galleryData, galleryDivWidth, marginPx);
+
+    return (
+        <>
+            <Routes>
+                <Route
+                    path=":imageName"
+                    element={
+                        <LightBox
+                            path={path}
+                            galleryData={galleryData}
+                            loadMoreImages={loadMoreImages}
+                        />
+                    }
+                />
+            </Routes>
+            {galleryData.imageList.map((image, index) =>
+                <GalleryThumb
+                    key={image.fileName}
+                    image={image}
+                    title={`${title} - ${image.fileName}`}
+                    marginPx={marginPx}
+                    resizeRatio={resizeRatios[index] || 1}
+                    path={path}
+                    ref={
+                        index === galleryData.imageList.length - threshold
+                            ? refTriggerLoadWhenVisible
+                            : null
+                    }
+                />
+            )}
+        </>
+    );
+};
+
+const getResizeRatios = (galleryData: GalleryData, divWidth: number, marginPx: number): number[] => {
+    let nextRowWidthOfThumbs = 0;
+    let nextRowImageCount = 0;
+    const ratios: number[] = [];
+
+    galleryData?.imageList?.forEach((image) => {
+        nextRowImageCount++;
+        nextRowWidthOfThumbs += image.thumbDimensions.width;
+        const widthAvailableForThumbs = divWidth - (2 * marginPx * nextRowImageCount);
+
+        if (nextRowWidthOfThumbs >= widthAvailableForThumbs) {
+            const resizeRatio = widthAvailableForThumbs / nextRowWidthOfThumbs;
+            ratios.push(...Array(nextRowImageCount).fill(resizeRatio));
+            nextRowWidthOfThumbs = 0;
+            nextRowImageCount = 0;
+        }
+    });
+
+    return ratios;
+};
