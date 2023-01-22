ededd3d85934f1db0e36c209e387574b22b8c2aa
Chris Elston <chris.elston@blueyonder.co.uk>
2023-01-22 17:24:02 +0000

    Site: Use axios instead of fetch for API calls
    Resolves #56

--- src/hooks/galleryQueries.ts
+++ src/hooks/galleryQueries.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 
 import { GalleryData } from '../types/Gallery';
@@ -5,18 +6,12 @@ import { GalleryData } from '../types/Gallery';
 const apiUrl = process.env.API_URL || '';
 
 const galleryListQuery = async (path: string, limit = 0): Promise<GalleryData> => {
-    const response = await fetch(`${apiUrl}/gallery/imagelist/${path}?limit=${limit}`);
-
-    if (!response.ok) throw new Error('Network Response was not Ok');
-
-    const returnData: GalleryData = await response.json();
-
-    return returnData || {};
+    const { data } = await axios.get<GalleryData>(`${apiUrl}/gallery/imagelist/${path}?limit=${limit}`);
+    return data;
 };
 
 export const useGalleryList = (path: string, limit = 0) => {
     const queryName = `gallery/imagelist/${path}`;
-
     return useQuery({
         queryKey: [queryName, limit],
         keepPreviousData: true,
--- src/hooks/markdownQueries.ts
+++ src/hooks/markdownQueries.ts
@@ -1,36 +1,25 @@
+import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 import { MdNavContents } from '../types/Markdown';
 
 const apiUrl = process.env.API_URL || '';
 
 const markdownFileQuery = async (path: string): Promise<string> => {
-    const response = await fetch(`${apiUrl}/markdown/mdfile/${path}`);
-
-    if (!response.ok) throw new Error('Network Response was not Ok');
-
-    const returnData: string = await response.text();
-
-    return returnData;
+    const { data } = await axios.get<string>(`${apiUrl}/markdown/mdfile/${path}`);
+    return data;
 };
 
 export const useMarkdownFile = (path: string) => {
     const queryName = `markdown/${path}`;
-
     return useQuery([queryName], () => markdownFileQuery(path));
 };
 
 const markdownNavQuery = async (path: string): Promise<MdNavContents> => {
-    const response = await fetch(`${apiUrl}/markdown/mdnav/${path}`);
-
-    if (!response.ok) throw new Error('Network Response was not Ok');
-
-    const returnData: MdNavContents = await response.json();
-
-    return returnData;
+    const { data } = await axios.get<MdNavContents>(`${apiUrl}/markdown/mdnav/${path}`);
+    return data;
 };
 
 export const useMarkdownNav = (path: string) => {
     const queryName = `markdown/${path}`;
-
     return useQuery([queryName], () => markdownNavQuery(path));
 };
