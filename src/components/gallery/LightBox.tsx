477c8e190aec6e93ffb8b937af5c9881183b0297
Chris Elston <chris.elston@blueyonder.co.uk>
2023-03-09 22:29:26 +0000

    LightBox: Fade out overlays
    Resolves #27

--- src/components/gallery/LightBox.tsx
+++ src/components/gallery/LightBox.tsx
@@ -21,18 +21,28 @@ export const LightBox: FC<LightBoxProps> = ({ parentTitle, currImage, prevImage,
         navigate('..', { replace: true });
     };
 
-    const goPrevImage = () => {
-        prevImage && navigate(`../${prevImage.fileName}`, { replace: true });
-    };
-
-    const goNextImage = () => {
-        nextImage && navigate(`../${nextImage.fileName}`, { replace: true });
-    };
-
     const handleOuterClick = (event: MouseEvent) => {
         if (event.target === event.currentTarget) {
             goBack();
         }
+    };
+
+    const restartFadeOut = () => {
+        const elements = document.querySelectorAll<HTMLElement>('.fadeOut');
+        elements.forEach((element) => {
+            element.classList.remove('fadeOut');
+            setTimeout(() => { element.classList.add('fadeOut'); }, 10);
+        });
+    };
+
+    const goPrevImage = () => {
+        prevImage && navigate(`../${prevImage.fileName}`, { replace: true });
+        restartFadeOut();
+    };
+
+    const goNextImage = () => {
+        nextImage && navigate(`../${nextImage.fileName}`, { replace: true });
+        restartFadeOut();
     };
 
     useKeyPress(['Backspace', 'Escape'], goBack);
@@ -41,13 +51,13 @@ export const LightBox: FC<LightBoxProps> = ({ parentTitle, currImage, prevImage,
     useKeyPress(['ArrowUp', 'ArrowDown', 'PageUp', 'PageDown', 'Home', 'End'], null);
     
     return (
-        <div className='LightBox' onClick={handleOuterClick}>
+        <div className='LightBox' onClick={handleOuterClick} onMouseMove={restartFadeOut}>
             <Helmet><title>{parentTitle} - {currImage.fileName}</title></Helmet>
-            <div className='close' onClick={goBack}>&times;</div>
-            {prevImage && <div className='prev' onClick={goPrevImage}>&#10094;</div>}
-            {nextImage && <div className='next' onClick={goNextImage}>&#10095;</div>}
+            <div className='close fadeOut' onClick={goBack}>&times;</div>
+            {prevImage && <div className='prev fadeOut' onClick={goPrevImage}>&#10094;</div>}
+            {nextImage && <div className='next fadeOut' onClick={goNextImage}>&#10095;</div>}
             <img src={currImage.fhdSrcUrl} alt={currImage.fileName} />
-            <div className='image-info'>{currImage.description}</div>
+            <div className='image-info fadeOut'>{currImage.description}</div>
 
             <div className='preload'>
                 {prevImage && <img src={prevImage.fhdSrcUrl} alt='preload' />}
