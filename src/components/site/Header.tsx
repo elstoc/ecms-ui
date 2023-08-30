81120c19b4a39d1cace5d9d579571dd5bb6ff613
Chris Elston <chris.elston@blueyonder.co.uk>
2023-08-30 23:03:22 +0100

    Improve header/footer layout

--- src/components/site/Header.tsx
+++ src/components/site/Header.tsx
@@ -1,13 +1,19 @@
 import React, { FC, ReactElement } from 'react';
 import { ComponentMetadata } from '../../types/Site';
 import { SiteNav } from './SiteNav';
+import { ToolBox } from './ToolBox';
 
 import './Header.css';
 
 export const Header: FC<{ componentMetadata: ComponentMetadata[]; }> = ({ componentMetadata }): ReactElement => {
     return (
         <div className='header'>
-            <SiteNav componentMetadata={componentMetadata}/>
+            <div className='header-sitenav'>
+                <SiteNav componentMetadata={componentMetadata}/>
+            </div>
+            <div className='header-toolbox'>
+                <ToolBox />
+            </div>
         </div>
     );
 };
