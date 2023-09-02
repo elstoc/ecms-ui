ff11879bd2329ea0041e527257104361a6463478
Chris Elston <chris.elston@blueyonder.co.uk>
2023-09-03 00:35:25 +0100

    Implement codemirror syntax-highlighting editor

--- src/components/markdown/MarkdownPageEdit.tsx
+++ src/components/markdown/MarkdownPageEdit.tsx
@@ -1,10 +1,14 @@
 import React, { FC, ReactElement } from 'react';
-import './MarkdownPageEdit.css';
+import CodeMirror from '@uiw/react-codemirror';
+import { markdown as codeMirrorMarkdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { languages } from '@codemirror/language-data';
+import { EditorView } from '@codemirror/view';
+import './MarkdownPageEdit.scss';
 
 export const MarkdownPageEdit: FC<{ markdown: string; }> = ({ markdown }): ReactElement => {
     return (
         <div className='markdown-page-edit'>
-            <textarea defaultValue={markdown} />
+            <CodeMirror height='100%' value={markdown} extensions={[codeMirrorMarkdown({ base: markdownLanguage, codeLanguages: languages}), EditorView.lineWrapping]} />
         </div>
     );
 };
