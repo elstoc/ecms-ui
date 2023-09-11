7f1c795bec28de96cfbac0296aa4eedb35594e87
Chris Elston <chris.elston@blueyonder.co.uk>
2023-09-11 20:05:26 +0100

    Icon: Accept optional onClick handler

--- src/components/site/Icon.tsx
+++ src/components/site/Icon.tsx
@@ -1,14 +1,20 @@
 // See https://react-icons.github.io/react-icons
 
 import React, { FC, ReactElement } from 'react';
-import { FiUser, FiUserX } from 'react-icons/fi';
+import { FiUser, FiUserX, FiX } from 'react-icons/fi';
 import { HiOutlineCodeBracket } from 'react-icons/hi2';
 
 import './Icon.css';
+import { IconType } from 'react-icons';
 
-export const Icon: FC<{ name: string }> = ({ name }): ReactElement => {
-    if (name === 'user') return <FiUser className='icon' />;
-    if (name === 'noUser') return <FiUserX className='icon'  />;
-    if (name === 'showSource') return <HiOutlineCodeBracket className='icon' />;
-    return <></>;
+export const Icon: FC<{ name: string, onClick?: () => void }> = ({ name, onClick }): ReactElement => {
+    let IconComponent: IconType | undefined;
+    if (name === 'user') IconComponent = FiUser;
+    if (name === 'noUser') IconComponent = FiUserX;
+    if (name === 'showSource') IconComponent = HiOutlineCodeBracket;
+    if (name === 'cancel') IconComponent = FiX;
+
+    return IconComponent
+        ? <IconComponent className='icon' onClick={onClick} />
+        : <></>;
 };
