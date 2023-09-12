e22f458177b35ba1d39551a42f57488dd92b5c08
Chris Elston <chris.elston@blueyonder.co.uk>
2023-09-12 23:23:56 +0100

    Change auth icon when logged in/out
    Resolves #71

--- src/components/site/ToolBox.tsx
+++ src/components/site/ToolBox.tsx
@@ -1,14 +1,20 @@
 import React, { FC, ReactElement } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Icon } from './Icon';
+import { useUserInfo } from '../../hooks/useUserInfo';
 
 import './ToolBox.scss';
 
 export const ToolBox: FC = (): ReactElement => {
+    const { data: userData } = useUserInfo();
+    const iconName = userData?.id && userData.id !== 'guest'
+        ? 'user'
+        : 'noUser';
+
     return (
         <div className='toolbox'>
             <NavLink to="auth/user">
-                <Icon name='user' />
+                <Icon name={iconName} />
             </NavLink>
         </div>
     );
