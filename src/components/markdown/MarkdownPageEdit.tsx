491eeed530e9802606b0b89a9960a44df4cfb037
Chris Elston <chris.elston@blueyonder.co.uk>
2023-09-13 21:56:07 +0100

    MarkdownPageEdit: Make edit box a controlled component

--- src/components/markdown/MarkdownPageEdit.tsx
+++ src/components/markdown/MarkdownPageEdit.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useCallback } from 'react';
+import React, { FC, ReactElement, useCallback, useState } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { markdown as codeMirrorMarkdown, markdownLanguage } from '@codemirror/lang-markdown';
 import { languages } from '@codemirror/language-data';
@@ -8,11 +8,12 @@ import { Icon } from '../site/Icon';
 import { useSearchParams } from 'react-router-dom';
 
 export const MarkdownPageEdit: FC<{ markdown: string; }> = ({ markdown }): ReactElement => {
+    const [editedMarkdown, setEditedMarkdown] = useState(markdown);
     const [, setSearchParams] = useSearchParams();
 
     const saveMd = useCallback(() => (
-        alert('not really saved but you get the idea')
-    ), []);
+        alert(`saving ${editedMarkdown.length} characters`)
+    ), [editedMarkdown]);
 
     const unsetEditMode = () => setSearchParams();
 
@@ -23,7 +24,12 @@ export const MarkdownPageEdit: FC<{ markdown: string; }> = ({ markdown }): React
                 <Icon name='save' onClick={saveMd} tooltipContent='save edited page'/>
             </div>
             <div className='markdown-page-edit'>
-                <CodeMirror height='100%' value={markdown} extensions={[codeMirrorMarkdown({ base: markdownLanguage, codeLanguages: languages}), EditorView.lineWrapping]} />
+                <CodeMirror
+                    height='100%'
+                    value={editedMarkdown}
+                    onChange={(value: string) => setEditedMarkdown(value)}
+                    extensions={[codeMirrorMarkdown({ base: markdownLanguage, codeLanguages: languages }), EditorView.lineWrapping]}
+                />
             </div>
         </>
     );
