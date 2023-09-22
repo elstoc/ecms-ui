ec2ccaa65e24c13356832802d10bce691da15dd3
Chris Elston <chris.elston@blueyonder.co.uk>
2023-09-23 00:28:09 +0100

    Site: Rename component route

--- src/api/site.ts
+++ src/api/site.ts
@@ -2,6 +2,6 @@ import { axiosSecureClient } from '../api/axiosClients';
 import { ComponentMetadata } from '../types/Site';
 
 export const getSiteNav = async (): Promise<ComponentMetadata[]> => {
-    const { data } = await axiosSecureClient.get<ComponentMetadata[]>('site/nav');
+    const { data } = await axiosSecureClient.get<ComponentMetadata[]>('site/components');
     return data;
 };
