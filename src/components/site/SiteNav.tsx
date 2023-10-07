e97d2c9084021efb6edd95340bc374627e939e6b
Chris Elston <chris.elston@blueyonder.co.uk>
2023-10-07 14:44:05 +0100

    SiteNav: Tweak style when only one component

--- src/components/site/SiteNav.tsx
+++ src/components/site/SiteNav.tsx
@@ -5,10 +5,12 @@ import { ComponentMetadata } from '../../types/Site';
 import './SiteNav.scss';
 
 export const SiteNav: FC<{ componentMetadata: ComponentMetadata[] }> = ({ componentMetadata }): ReactElement => {
+    const className = componentMetadata.length === 1 ? 'single-component' : '';
+
     return (
         <div className='sitenav'>
             {componentMetadata.map((props) =>
-                <NavLink to={props.uiPath} key={props.apiPath}>{props.title}</NavLink>
+                <NavLink className={className} to={props.uiPath} key={props.apiPath}>{props.title}</NavLink>
             )}
         </div>
     );
