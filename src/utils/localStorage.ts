b88b786db95c74d7a66cf4fbec1f61478ec4c650
Chris Elston <chris.elston@blueyonder.co.uk>
2024-01-21 15:52:00 +0000

    Use cookies to store access token
    This fixes (or attempts to fix) part of #92

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,8 +1,11 @@
+import cookies from 'js-cookie';
+
 export const setStorage = (key: string, value: string): void => {
     localStorage.setItem(key, value);
+    cookies.set(key, value, { expires: 7, path: '' });
     window.dispatchEvent(new Event('storage'));
 };
 
 export const getStorage = (key: string): string => {
-    return localStorage.getItem(key) ?? '';
+    return cookies.get(key) ?? '';
 };
