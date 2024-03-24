cd6982c6687d38244c3f75efceff0d47cdbdcc9d
Chris Elston <chris.elston@blueyonder.co.uk>
2024-03-24 11:03:28 +0000

    Gallery: Remove wildcard path params

--- src/api/gallery.ts
+++ src/api/gallery.ts
@@ -2,7 +2,7 @@ import { GalleryContents } from '../types/Gallery';
 import { axiosSecureClient } from '../api/axiosClients';
 
 export const getGalleryContents = async (path: string, limit = 0): Promise<GalleryContents> => {
-    const urlPath = `gallery/contents/${path}?limit=${limit}`;
-    const { data } = await axiosSecureClient.get<GalleryContents>(urlPath);
+    const url = 'gallery/contents/';
+    const { data } = await axiosSecureClient.get<GalleryContents>(url, { params: { path, limit }});
     return data;
 };
