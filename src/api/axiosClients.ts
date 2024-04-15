93df0b3997fb8a62b74233bfc909e8d11bab709f
Chris Elston <chris.elston@blueyonder.co.uk>
2024-04-15 14:22:54 +0100

    Implement proactive auth token refresh
    Resolves #95

--- src/api/axiosClients.ts
+++ src/api/axiosClients.ts
@@ -11,50 +11,39 @@ const axiosDefaults = {
 export const axiosClient = axios.create(axiosDefaults);
 export const axiosSecureClient = axios.create(axiosDefaults);
 
-const axiosSecureClientRetry = axios.create(axiosDefaults);
+let fetchingNewTokens = false;
+let waitingForTokensQueue: (() => void)[] = [];
 
-const injectAccessToken = async (config: InternalAxiosRequestConfig<unknown>) => {
+const finishWaitingRequests = () => {
+    fetchingNewTokens = false;
+    waitingForTokensQueue.forEach((cb) => cb());
+    waitingForTokensQueue = [];
+};
+
+const resolveWithConfig = (config: InternalAxiosRequestConfig<unknown>, cb: (config: InternalAxiosRequestConfig<unknown>) => void) => {
     const { accessToken } = getAccessToken();
     if (accessToken) {
         config.headers['authorization'] = `Bearer ${accessToken}`;
     }
     config.url = config.url?.replace(/\/$/, '');
-    return config;
+    cb(config);
 };
 
-axiosSecureClient.interceptors.request.use(injectAccessToken);
-axiosSecureClientRetry.interceptors.request.use(injectAccessToken);
-
-let failedRequestRetryQueue: (() => void)[] = [];
-
-const addFailedRequestToRetryQueue = (resolve: (value: unknown) => void, config: InternalAxiosRequestConfig<unknown>): void => {
-    failedRequestRetryQueue.push(() => {
-        resolve(axiosSecureClientRetry(config));
+const injectAccessToken = (config: InternalAxiosRequestConfig<unknown>): Promise<InternalAxiosRequestConfig> => {
+    return new Promise((resolve) => {
+        const { accessTokenExpiry } = getAccessToken();
+        const tokenExpired = accessTokenExpiry && (accessTokenExpiry * 1000 < Date.now() + 10000);
+        if (!tokenExpired) {
+            resolveWithConfig(config, resolve);
+        } else {
+            waitingForTokensQueue.push(() => resolveWithConfig(config, resolve));
+            if (!fetchingNewTokens) {
+                fetchingNewTokens = true;
+                refreshAccessToken()
+                    .then(() => finishWaitingRequests());
+            }
+        }
     });
 };
 
-let fetchingNewToken = false;
-
-const retryFailedRequests = () => {
-    fetchingNewToken = false;
-    failedRequestRetryQueue.forEach((callback) => callback());
-    failedRequestRetryQueue = [];
-};
-
-axiosSecureClient.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        return new Promise((resolve, reject) => {
-            if (error.response?.status === 401) {
-                addFailedRequestToRetryQueue(resolve, error.config);
-                if (fetchingNewToken)
-                    return;
-                fetchingNewToken = true;
-                refreshAccessToken()
-                    .then(() => retryFailedRequests());
-            } else {
-                reject(error);
-            }
-        });
-    }
-);
+axiosSecureClient.interceptors.request.use(injectAccessToken);
