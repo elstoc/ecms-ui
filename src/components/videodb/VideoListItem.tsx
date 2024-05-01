4ed0db06d5ecbeca10ce4a65e696779306f7fc35
Chris Elston <chris.elston@blueyonder.co.uk>
2024-05-01 22:53:39 +0100

    VideoDb: Refactor and add final lookup

--- src/components/videodb/VideoListItem.tsx
+++ src/components/videodb/VideoListItem.tsx
@@ -13,15 +13,22 @@ type VideoDbProps = {
 export const VideoListItem: FC<VideoDbProps> = ({ apiPath, video }): ReactElement => {
     const [, categoryLookup] = useVideoDbLookup(apiPath, 'categories');
     const [, watchedStatusLookup] = useVideoDbLookup(apiPath, 'watched_status');
+    const [, mediaTypeLookup] = useVideoDbLookup(apiPath, 'media_types');
+
+    const category = categoryLookup?.[video.category];
+    const watchedStatus = watchedStatusLookup?.[video.watched];
+    const pmWatchedStatus = watchedStatusLookup?.[video.pm_watched];
+    const pMediaType = mediaTypeLookup?.[video.pm_media_type];
+
     return (
         <div className='video-list-item'>
             <div className='video-name'>{ video.title }</div>
             <div className='sub-info'>
-                <span className='category'>{categoryLookup?.[video.category]}</span>
+                <span className='category'>{category}</span>
                 {video.length_mins > 0 && <span> | {video.length_mins} mins</span>}
-                {video.watched && <span> | watched: {watchedStatusLookup?.[video.watched]}</span>}
+                {video.watched && <span> | watched: {watchedStatus}</span>}
                 {video.to_watch_priority && <span> | to Watch Priority: {video.to_watch_priority}</span>}
-                <span> | {video.pm_media_type} ({watchedStatusLookup?.[video.pm_watched]})</span>
+                <span> | {pMediaType} ({pmWatchedStatus})</span>
             </div>
         </div>
     );
