f186075a86835023cc90d8aea8441e00c5b0272c
Chris Elston <chris.elston@blueyonder.co.uk>
2024-05-12 00:14:43 +0100

    SiteRoutes: Minor refactor

--- src/components/site/SiteRoutes.tsx
+++ src/components/site/SiteRoutes.tsx
@@ -11,51 +11,45 @@ import { useSiteConfig } from '../../hooks/useApiQueries';
 export const SiteRoutes: FC<{ componentMetadata: ComponentMetadata[] }> = ({ componentMetadata }): ReactElement => {
     const [, siteConfig] = useSiteConfig();
 
-    const siteRoutes = componentMetadata.map((component) => {
-        if (component.type === 'gallery') {
-            return (
-                <Route
-                    key={component.apiPath}
-                    path={`${component.uiPath}/*`}
-                    element={<Gallery
-                        apiPath={component.apiPath}
-                        title={component.title}
+    const siteRoutes = componentMetadata
+        .map((component) => {
+            const { type, uiPath, apiPath, title } = component;
+            const pathAndTitle = { apiPath, title };
+
+            let element: React.JSX.Element;
+            if (type === 'gallery') {
+                element = (
+                    <Gallery
+                        {...pathAndTitle}
                         marginPx={component.additionalData.marginPx as number}
                         batchSize={component.additionalData.batchSize as number}
                         threshold={component.additionalData.threshold as number}
-                    />}
-                />
-            );
-        } else if (component.type === 'markdown') {
-            return (
-                <Route
-                    key={component.apiPath}
-                    path={`${component.uiPath}/*`}
-                    element={<Markdown
-                        apiPath={component.apiPath}
-                        title={component.title}
+                    />
+                );
+            } else if (type === 'markdown') {
+                element = (
+                    <Markdown
+                        {...pathAndTitle}
                         includeNav={component.additionalData.includeNav as boolean}
-                    />}
-                />
-            );
-        } else if (component.type === 'videodb') {
-            return (
-                <Route
-                    key={component.apiPath}
-                    path={`${component.uiPath}/*`}
-                    element={<VideoDb
-                        apiPath={component.apiPath}
-                        title={component.title}
-                    />}
-                />
-            );
-        }
-        throw new Error(`incompatible component type ${component.type}`);
-    });
+                    />
+                );
+            } else if (type === 'videodb') {
+                element = (
+                    <VideoDb {...pathAndTitle} />
+                );
+            } else {
+                throw new Error(`incompatible component type ${type}`);
+            }
+
+            return <Route key={apiPath} path={`${uiPath}/*`} element={element} />;
+        });
+
     return (
         <Routes>
             {siteRoutes}
-            {siteConfig?.authEnabled && <Route path='auth/*' element={<Auth />} />}
+            {siteConfig?.authEnabled &&
+                <Route path='auth/*' element={<Auth />} />
+            }
             <Route path='*' element={<Navigate to='/' />} />
         </Routes>
     );
