d9ceb319f587fee23771163aead710156b7ffbfa
Chris Elston <chris.elston@blueyonder.co.uk>
2024-05-20 10:05:33 +0100

    Wrap VideoDb component in Suspense

--- src/components/videodb/VideoDb.tsx
+++ src/components/videodb/VideoDb.tsx
@@ -1,42 +1,10 @@
-import React, { FC, ReactElement } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import React, { FC, ReactElement, Suspense } from 'react';
+import { VideoDbProps, VideoDbContent } from './VideoDbContent';
 
-import { useVideoDbVideos } from '../../hooks/useApiQueries';
-import { HandleQueryState } from '../utils/HandleQueryState';
-import { VideoDbList } from './VideoDbList';
-import { VideoQueryParams } from './VideoQueryParams';
-import { ViewEditVideo } from './ViewEditVideo';
-
-type VideoDbProps = {
-    apiPath: string;
-    title: string;
-}
-
-export const VideoDb: FC<VideoDbProps> = ({ apiPath, title }): ReactElement => {
-    const [searchParams] = useSearchParams();
-    const { maxLength, titleLike, categories, id } = Object.fromEntries([...searchParams]);
-
-    const videoQueryParams: { [key: string]: string } = {};
-    if (maxLength !== undefined) {
-        videoQueryParams.maxLength = maxLength;
-    }
-    if (titleLike !== undefined) {
-        videoQueryParams.titleLike = titleLike;
-    }
-    if (categories !== undefined) {
-        videoQueryParams.categories = categories;
-    }
-    const [ queryState, videos ] = useVideoDbVideos(apiPath, videoQueryParams);
-
+export const VideoDb: FC<VideoDbProps> = (props): ReactElement => {
     return (
-        <div className='videodb'>
-            <Helmet><title>{title}</title></Helmet>
-            <HandleQueryState {...queryState}>
-                <ViewEditVideo apiPath={apiPath} videoId={id === undefined ? undefined : parseInt(id)} />
-                <VideoQueryParams apiPath={apiPath} />
-                {videos && <VideoDbList videos={videos} apiPath={apiPath} />}
-            </HandleQueryState>
-        </div>
+        <Suspense fallback='Loading...'>
+            <VideoDbContent {...props} />
+        </Suspense>
     );
 };
---
+++ src/components/videodb/VideoDbContent.tsx
@@ -0,0 +1,42 @@
+import React, { FC, ReactElement } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
+import { useVideoDbVideos } from '../../hooks/useApiQueries';
+import { HandleQueryState } from '../utils/HandleQueryState';
+import { VideoDbList } from './VideoDbList';
+import { VideoQueryParams } from './VideoQueryParams';
+import { ViewEditVideo } from './ViewEditVideo';
+
+export type VideoDbProps = {
+    apiPath: string;
+    title: string;
+}
+
+export const VideoDbContent: FC<VideoDbProps> = ({ apiPath, title }): ReactElement => {
+    const [searchParams] = useSearchParams();
+    const { maxLength, titleLike, categories, id } = Object.fromEntries([...searchParams]);
+
+    const videoQueryParams: { [key: string]: string } = {};
+    if (maxLength !== undefined) {
+        videoQueryParams.maxLength = maxLength;
+    }
+    if (titleLike !== undefined) {
+        videoQueryParams.titleLike = titleLike;
+    }
+    if (categories !== undefined) {
+        videoQueryParams.categories = categories;
+    }
+    const [ queryState, videos ] = useVideoDbVideos(apiPath, videoQueryParams);
+
+    return (
+        <div className='videodb'>
+            <Helmet><title>{title}</title></Helmet>
+            <HandleQueryState {...queryState}>
+                <ViewEditVideo apiPath={apiPath} videoId={id === undefined ? undefined : parseInt(id)} />
+                <VideoQueryParams apiPath={apiPath} />
+                {videos && <VideoDbList videos={videos} apiPath={apiPath} />}
+            </HandleQueryState>
+        </div>
+    );
+};
