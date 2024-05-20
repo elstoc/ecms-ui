3c8eb49d10c6f5cd3ca5fc700c9eb6f117865812
Chris Elston <chris.elston@blueyonder.co.uk>
2024-05-20 10:37:07 +0100

    Wrap Auth component in Suspense

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,25 +1,11 @@
-import React, { FC, ReactElement } from 'react';
-import { Route, Routes } from 'react-router';
-import { Helmet } from 'react-helmet';
+import React, { FC, ReactElement, Suspense } from 'react';
 
-import { useUserInfo } from '../../hooks/useApiQueries';
-import { Login } from './Login';
-import { Welcome } from './Welcome';
-import './Auth.css';
-import { HandleQueryState } from '../utils/HandleQueryState';
+import { AuthContent } from './AuthContent';
 
 export const Auth: FC = (): ReactElement => {
-    const [ queryState, userData ] = useUserInfo();
-
     return (
-        <div className='auth-container'>
-            <Helmet><title>User ({userData?.id ?? ''})</title></Helmet>
-            <HandleQueryState {...queryState}>
-                <Routes>
-                    <Route path="user" element={<Welcome user={userData?.id ?? ''} />} />
-                    <Route path="login" element={<Login />} />
-                </Routes>
-            </HandleQueryState>
-        </div>
+        <Suspense fallback='Loading...'>
+            <AuthContent />
+        </Suspense>
     );
 };
