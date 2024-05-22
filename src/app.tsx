ac40878d208384f9aec07e7e6188646fe81b8d51
Chris Elston <chris.elston@blueyonder.co.uk>
2024-05-22 19:25:59 +0100

    Looks like blueprintjs can live without icons (for now)

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,6 @@ import { FocusStyleManager } from '@blueprintjs/core';
 import './app.scss';
 import 'modern-normalize';
 import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 FocusStyleManager.onlyShowFocusOnTabs();
 
