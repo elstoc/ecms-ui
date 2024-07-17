245a74258c30b093cf2cd992328f6535fca6101a
Chris Elston <chris.elston@blueyonder.co.uk>
2024-07-17 20:44:21 +0100

    VideoDb: Some layout tweaks

--- src/videodb/components/EditMedium.tsx
+++ src/videodb/components/EditMedium.tsx
@@ -18,21 +18,21 @@ export const EditMedium: FC<EditMediumParams> = ({ medium, onChange, onDelete })
             <ControlGroup vertical={true}>
                 <ControlGroup fill={true} className='compress'>
                     <SelectLookup
-                        label=''
+                        label='Type'
                         lookupTable='media_types'
                         selectedKey={medium.media_type}
                         onSelectionChange={(media_type) => onChange?.({...medium, media_type})}
                         small={true}
                     />
                     <SelectLookup
-                        label=''
+                        label='Location'
                         lookupTable='media_locations'
                         selectedKey={medium.media_location}
                         onSelectionChange={(media_location) => onChange?.({...medium, media_location})}
                         small={true}
                     />
                     <SelectLookup
-                        label=''
+                        label='Watched'
                         lookupTable='watched_status'
                         selectedKey={medium.watched}
                         onSelectionChange={(watched) => onChange?.({...medium, watched})}
@@ -41,7 +41,7 @@ export const EditMedium: FC<EditMediumParams> = ({ medium, onChange, onDelete })
                     <Button small={true} icon='delete' onClick={() => onDelete?.()} />
                 </ControlGroup>
                 <NullableStringInput
-                    label=''
+                    label='Notes'
                     value={medium.notes}
                     small={true}
                     onValueChange={(notes) => onChange?.({...medium, notes})}
