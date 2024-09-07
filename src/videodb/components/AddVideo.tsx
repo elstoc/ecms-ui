41f69f64f27ddbf3f2c655e57ec7a3febc24cf1a
Chris Elston <chris.elston@blueyonder.co.uk>
2024-09-07 10:42:09 +0100

    AddVideo: Refactor

--- src/videodb/components/AddVideo.tsx
+++ src/videodb/components/AddVideo.tsx
@@ -3,32 +3,35 @@ import { useNavigate } from 'react-router-dom';
 import { Dialog, DialogBody } from '@blueprintjs/core';
 import { useQueryClient } from '@tanstack/react-query';
 
-import { EditVideoForm } from './EditVideoForm';
-import { VideoDbContext } from '../hooks/useVideoDbState';
 import { postVideoDbVideo, Video, VideoWithId } from '../api';
+import { VideoDbContext } from '../hooks/useVideoDbState';
+
+import { EditVideoForm } from './EditVideoForm';
 import { AppToaster } from '../../common/components/toaster';
+
+const initialVideo = {
+    id: 0, title: '', category: '',
+    director: null, length_mins: null, watched: '', to_watch_priority: null,
+    progress: null, imdb_id: null, image_url: null, year: null,
+    actors: null, plot: null, tags: [],
+    primary_media_type: null, primary_media_location: null,
+    primary_media_watched: null, other_media_type: null,
+    other_media_location: null, media_notes: null,
+};
 
 export const AddVideo: FC = (): ReactElement => {
     const navigate = useNavigate();
-    const { state: { apiPath } } = useContext(VideoDbContext);
-    const initialVideo = {
-        id: 0, title: '', category: '',
-        director: null, length_mins: null, watched: '', to_watch_priority: null,
-        progress: null, imdb_id: null, image_url: null, year: null,
-        actors: null, plot: null, tags: [],
-        primary_media_type: null, primary_media_location: null,
-        primary_media_watched: null, other_media_type: null,
-        other_media_location: null, media_notes: null,
-    };
     const queryClient = useQueryClient();
 
-    const saveVideo = useCallback(async (video: VideoWithId) => {
+    const { state: { apiPath } } = useContext(VideoDbContext);
+
+    const addVideo = useCallback(async (video: VideoWithId) => {
         try {
             const videoWithoutId = { ...video, id: undefined } as Video;
             await postVideoDbVideo(apiPath, videoWithoutId);
-            queryClient.invalidateQueries({ queryKey: ['videoDb', 'videos']});
-            queryClient.invalidateQueries({ queryKey: ['videoDb', 'tags']});
             (await AppToaster).show({ message: 'saved', timeout: 2000 });
+            await queryClient.invalidateQueries({ queryKey: ['videoDb', 'videos']});
+            await queryClient.invalidateQueries({ queryKey: ['videoDb', 'tags']});
             navigate(-1);
         } catch (error: unknown) {
             alert('error ' + error);
@@ -44,7 +47,7 @@ export const AddVideo: FC = (): ReactElement => {
             className='update-video'
         >
             <DialogBody useOverflowScrollContainer={false}>
-                <EditVideoForm initialVideoState={initialVideo} onSave={saveVideo} />
+                <EditVideoForm initialVideoState={initialVideo} onSave={addVideo} />
             </DialogBody>
         </Dialog>
     );
