23a5fed2c9921e4233d8e46b856569a6b95d1c85
Chris Elston <chris.elston@blueyonder.co.uk>
2024-09-07 10:56:03 +0100

    VideoDb: Refactor

--- src/videodb/components/VideoDb.tsx
+++ src/videodb/components/VideoDb.tsx
@@ -1,8 +1,9 @@
 import React, { FC, ReactElement, Suspense} from 'react';
 
 import { VideoDbComponentMetadata } from '../../site/api';
-import { VideoDbContent } from './VideoDbContent';
 import { VideoDbContext, initialFilters, useVideoDbState } from '../hooks/useVideoDbState';
+
+import { VideoDbContent } from './VideoDbContent';
 
 export const VideoDb: FC<VideoDbComponentMetadata> = ({ title, apiPath }): ReactElement => {
     const initialState = { title, apiPath, filters: initialFilters, pendingFlagUpdates: [] };
