e98f1573fe5977a1a75a87a2067c4168684c66dd
Chris Elston <chris.elston@blueyonder.co.uk>
2024-09-18 10:19:36 +0100

    GalleryContent: Some minor tweaks

--- src/gallery/components/GalleryContent.tsx
+++ src/gallery/components/GalleryContent.tsx
@@ -1,11 +1,12 @@
 import React, { FC, ReactElement, useContext } from 'react';
 
+import { useTitle } from '../../common/hooks';
+
 import { JustifiedGallery } from './JustifiedGallery';
 import { GalleryStateContext } from './Gallery';
 import { GalleryLightBox } from './GalleryLightBox';
-import { useTitle } from '../../common/hooks';
 
-import './GalleryContent.css';
+import './GalleryContent.scss';
 
 export const GalleryContent: FC = (): ReactElement => {
     const { galleryState: { title } } = useContext(GalleryStateContext);
