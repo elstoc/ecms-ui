6db038f85aadd0d11c811c26b0655871ae677cef
Chris Elston <chris.elston@blueyonder.co.uk>
2024-10-01 15:37:21 +0100

    VideoDb: Further refactor of watched icons
     - combine both icons into a single componenet
     - add tooltip
    
    Resolves #172

--- src/videodb/components/VideoListItem.tsx
+++ src/videodb/components/VideoListItem.tsx
@@ -7,7 +7,7 @@ import { useUserIsAdmin } from '../../auth/hooks/useAuthQueries';
 import { VideoDbContext } from '../hooks/useVideoDbState';
 import { VideoWithId } from '../api';
 
-import { WatchedIcon } from './WatchedIcon';
+import { WatchedIcons } from './WatchedIcons';
 import { Flag } from '../../common/components/forms';
 import { Icon } from '../../common/components/icon';
 
@@ -60,8 +60,7 @@ export const VideoListItem = forwardRef<HTMLDivElement, { video: VideoWithId }>(
                 <div className='left'>
                     <div className='video-title'>{video.title}</div>
                     <div>
-                        <WatchedIcon watchedStatus={video.watched} />
-                        <WatchedIcon watchedStatus={video.primary_media_watched} />
+                        <WatchedIcons watchedStatus={video.watched} mediaWatchedStatus={video.primary_media_watched} mediaDesc={primaryMediaType}/>
                         <span> {primaryMediaType} {lengthText}</span>
                     </div>
                 </div>
