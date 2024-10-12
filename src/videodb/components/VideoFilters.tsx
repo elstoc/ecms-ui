66bcbb101c8c26404dab7e1148c8c2e07963e95d
Chris Elston <chris.elston@blueyonder.co.uk>
2024-10-13 00:05:17 +0100

    Remove Card styling from VideoFilters

--- src/videodb/components/VideoFilters.tsx
+++ src/videodb/components/VideoFilters.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactElement } from 'react';
-import { Button, Card } from '@blueprintjs/core';
+import { Button } from '@blueprintjs/core';
 
 import { useVideoDbFilterState } from '../hooks/useVideoDbFilterState';
 
@@ -27,71 +27,69 @@ export const VideoFilters: FC = (): ReactElement => {
 
     return (
         <div className='video-filters'>
-            <Card className='card'>
-                <NullableSelectLookup
-                    label='Category'
-                    className='category'
-                    lookupTable='categories'
-                    inline={true}
-                    selectedKey={categories}
-                    onSelectionChange={(value) => updateState({ action: 'setFilter', key: 'categories', value })}
-                    nullValueRepr='All'
-                    filterable={false}
-                />
-                <SegmentedControlInput
-                    label='Min Resolution'
-                    inline={true}
-                    options={minResolutionOptions}
-                    value={minResolution || 'SD'}
-                    onValueChange={(value) => updateState({ action: 'setFilter', key: 'minResolution', value })}
-                />
-                <SegmentedControlInput
-                    label='Watched'
-                    inline={true}
-                    options={watchedStatusOptions}
-                    value={watched ?? 'All'}
-                    onValueChange={(value) => updateState({ action: 'setFilter', key: 'watched', value })}
-                />
-                <SegmentedControlInput
-                    label='Media Watched'
-                    inline={true}
-                    options={watchedStatusOptions}
-                    value={mediaWatched ?? 'All'}
-                    onValueChange={(value) => updateState({ action: 'setFilter', key: 'mediaWatched', value })}
-                />
-                <NullableIntInput
-                    label='Max Length' 
-                    className='max-length'
-                    inline={true}
-                    value={maxLength}
-                    onValueChange={(value) => updateState({ action: 'setFilter', key: 'maxLength', value })}
-                />
-                <TagInput
-                    label='Tags'
-                    className='tags'
-                    inline={true}
-                    tags={tags}
-                    allowCreation={false}
-                    onSelectionChange={(value) => updateState({ action: 'setFilter', key: 'tags', value })}
-                />
-                <NullableStringInput
-                    label='Title Search'
-                    inline={true}
-                    value={titleContains}
-                    placeholder=''
-                    onValueChange={(value) => updateState({ action: 'setFilter', key: 'titleContains', value })}
-                />
-                <Switch
-                    label='Priority First'
-                    className='priority'
-                    inline={true}
-                    value={sortPriorityFirst === 1}
-                    onValueChange={(value) => updateState({action: 'setFilter', key: 'sortPriorityFirst', value: value ? 1 : 0})}
-                />
-                <div className='filter-action-buttons'>
-                    <Button onClick={clearAllFilters}>Reset Filters</Button>
-                </div>
-            </Card>
+            <NullableSelectLookup
+                label='Category'
+                className='category'
+                lookupTable='categories'
+                inline={true}
+                selectedKey={categories}
+                onSelectionChange={(value) => updateState({ action: 'setFilter', key: 'categories', value })}
+                nullValueRepr='All'
+                filterable={false}
+            />
+            <SegmentedControlInput
+                label='Min Resolution'
+                inline={true}
+                options={minResolutionOptions}
+                value={minResolution || 'SD'}
+                onValueChange={(value) => updateState({ action: 'setFilter', key: 'minResolution', value })}
+            />
+            <SegmentedControlInput
+                label='Watched'
+                inline={true}
+                options={watchedStatusOptions}
+                value={watched ?? 'All'}
+                onValueChange={(value) => updateState({ action: 'setFilter', key: 'watched', value })}
+            />
+            <SegmentedControlInput
+                label='Media Watched'
+                inline={true}
+                options={watchedStatusOptions}
+                value={mediaWatched ?? 'All'}
+                onValueChange={(value) => updateState({ action: 'setFilter', key: 'mediaWatched', value })}
+            />
+            <NullableIntInput
+                label='Max Length' 
+                className='max-length'
+                inline={true}
+                value={maxLength}
+                onValueChange={(value) => updateState({ action: 'setFilter', key: 'maxLength', value })}
+            />
+            <TagInput
+                label='Tags'
+                className='tags'
+                inline={true}
+                tags={tags}
+                allowCreation={false}
+                onSelectionChange={(value) => updateState({ action: 'setFilter', key: 'tags', value })}
+            />
+            <NullableStringInput
+                label='Title Search'
+                inline={true}
+                value={titleContains}
+                placeholder=''
+                onValueChange={(value) => updateState({ action: 'setFilter', key: 'titleContains', value })}
+            />
+            <Switch
+                label='Priority First'
+                className='priority'
+                inline={true}
+                value={sortPriorityFirst === 1}
+                onValueChange={(value) => updateState({action: 'setFilter', key: 'sortPriorityFirst', value: value ? 1 : 0})}
+            />
+            <div className='filter-action-buttons'>
+                <Button onClick={clearAllFilters}>Reset Filters</Button>
+            </div>
         </div>
     );
 };
