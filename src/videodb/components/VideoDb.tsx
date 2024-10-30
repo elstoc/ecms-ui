6e10bb4d533aa46b959c19d30b79550bea6190e5
Chris Elston <chris.elston@blueyonder.co.uk>
2024-10-30 22:53:50 +0000

    VideoDb: Move layout component higher in tree

--- src/videodb/components/VideoDb.tsx
+++ src/videodb/components/VideoDb.tsx
@@ -1,17 +1,45 @@
 import React, { FC, ReactElement, Suspense} from 'react';
 
+import { useTitle } from '../../shared/hooks';
 import { VideoDbMetadata } from '../../site/api';
 import { VideoDbStateContext, useVideoDbState } from '../hooks/useVideoDbStateContext';
 
 import { VideoDbContent } from './VideoDbContent';
+import { VideoToolbox } from './VideoToolbox';
+import { VideoFilters } from './VideoFilters';
+import { ContentWithSidebar } from '../../shared/components/layout';
 
 export const VideoDb: FC<VideoDbMetadata> = ({ title, apiPath }): ReactElement => {
     const videoDbState = useVideoDbState(title, apiPath);
 
+    useTitle(title);
+
+    const toolbar = (
+        <Suspense>
+            <VideoToolbox />
+        </Suspense>
+    );
+
+    const filters = (
+        <Suspense>
+            <VideoFilters />
+        </Suspense>
+    );
+
+    const content = (
+        <Suspense>
+            <VideoDbContent />
+        </Suspense>
+    );
+
     return (
         <VideoDbStateContext.Provider value={videoDbState} >
             <Suspense>
-                <VideoDbContent />
+                <ContentWithSidebar
+                    content={content}
+                    sidebar={filters}
+                    toolbarIcons={toolbar}
+                />
             </Suspense>
         </VideoDbStateContext.Provider>
     );
--- src/videodb/components/VideoDbContent.tsx
+++ src/videodb/components/VideoDbContent.tsx
@@ -1,48 +1,20 @@
-import React, { FC, ReactElement, Suspense, useContext } from 'react';
+import React, { FC, ReactElement, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { useTitle } from '../../shared/hooks';
 import { useGetUserIsAdmin } from '../../auth/hooks/useAuthQueries';
-import { VideoDbStateContext } from '../hooks/useVideoDbStateContext';
 
-import { ContentWithSidebar } from '../../shared/components/layout';
 import { VideoList } from './VideoList';
-import { VideoFilters } from './VideoFilters';
 import { UpdateVideo } from './UpdateVideo';
 import { AddVideo } from './AddVideo';
-import { VideoToolbox } from './VideoToolbox';
 
 import './VideoDbContent.scss';
 
 export const VideoDbContent: FC = (): ReactElement => {
-    const { videoDbState: { title } } = useContext(VideoDbStateContext);
     const userIsAdmin = useGetUserIsAdmin();
-
-    useTitle(title);
-
-    const toolbar = (
-        <Suspense>
-            <VideoToolbox />
-        </Suspense>
-    );
-
-    const filters = (
-        <Suspense>
-            <VideoFilters />
-        </Suspense>
-    );
-
-    const list = (
-        <Suspense>
-            <div className='video-content'>
-                <VideoList />
-            </div>
-        </Suspense>
-    );
 
     // suspense is wrapped around routes and page elements separately to stop screen flashing
     return (
-        <>
+        <div className='video-content'>
             {userIsAdmin &&
                 <Suspense>
                     <Routes>
@@ -51,11 +23,7 @@ export const VideoDbContent: FC = (): ReactElement => {
                     </Routes>
                 </Suspense>
             }
-            <ContentWithSidebar
-                content={list}
-                sidebar={filters}
-                toolbarIcons={toolbar}
-            />
-        </>
+            <VideoList />
+        </div>
     );
 };
